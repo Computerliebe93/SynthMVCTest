e5f8a3731f3f84de9f2141c49e4ce850207ca80e
Computerliebe93 <70689092+Computerliebe93@users.noreply.github.com>
2020-11-11 13:57:02 +0100

    Forsøg på inheritance og multithread.

---
+++ MultithreadGranular/src/Main.java
@@ -0,0 +1,76 @@
+import net.beadsproject.beads.core.AudioContext;
+import net.beadsproject.beads.data.Sample;
+import net.beadsproject.beads.ugens.GranularSamplePlayer;
+import net.beadsproject.beads.ugens.SamplePlayer;
+import net.beadsproject.beads.ugens.Static;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Grain g1, g2;
+        g2 = new Player.Controller();
+        g1 = new Grain() {
+            @Override
+            void sample() {
+            }
+        };
+        g1 = new Player();
+        g1.sample();
+        g2.sample();
+    }
+}
+
+abstract class Grain {
+    abstract void sample();
+    AudioContext ac = new AudioContext();
+}
+
+
+class Player extends Grain {
+    @Override
+    void sample() {
+        AudioContext ac = new AudioContext();
+        Sample sourceSample = null;
+        boolean sampleReady = false;
+        try {
+            sourceSample = new Sample("bells.wav");
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
+        GranularSamplePlayer gsp = new GranularSamplePlayer(ac, sourceSample);
+        gsp.setLoopType(SamplePlayer.LoopType.LOOP_FORWARDS);
+
+        ac.out.addInput(gsp);
+        ac.start();
+
+    }
+
+    static class Controller extends Player {
+        @Override
+        void sample() {
+            Sample sourceSample = null;
+            boolean sampleReady = false;
+            sampleReady = true;
+            GranularSamplePlayer gsp = new GranularSamplePlayer(ac, sourceSample);
+            while (sampleReady) {
+
+                System.out.println("Ready!");
+                Scanner scanner = new Scanner(System.in);
+                float ScannerFloat = scanner.nextFloat();
+                System.out.println("ScannerFloat is set to: " + ScannerFloat);
+                gsp.setGrainSize(new Static(ac, 500));
+
+                gsp.setPitch(new Static(ac, ScannerFloat));
+
+            }
+        }
+    }
+}
+
+
---
+++ src/com/g223/Synth/utils/RefWrapper.java
@@ -0,0 +1,11 @@
+package com.g223.Synth.utils;
+
+public class RefWrapper<T>
+{
+    public T val;
+
+    public RefWrapper(T val)
+    {
+        this.val = val;
+    }
+}
